ad8bd8ca117556b7fdd619d65f24991a8eddc89a
Roman <rlsempire@outlook.de>
2025-01-13 19:08:42 +0100

    first commit

---
+++ lessons/lessons/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Good Day!");
+Console.WriteLine("What is your name?");
+Console.WriteLine("\twhat's a wonderful day?\n");
+Console.WriteLine("agree!");
+int number = 1;
+Console.WriteLine(number);
+int number2 = 99;
+number2 = 100;
+Console.WriteLine(number2);
+int number3 = 0;
+number3 = 4;
+int number4 = 2;
+int sum = number3 + number4;
+Console.WriteLine(sum);
+int number5 = 0;
+int number6 = 2;
+int sum1 = number5 - number6;
+Console.WriteLine(sum1);
+int number7 = 100;
+int number8 = 1000;
+int sum2 = number7 * number8;
+Console.WriteLine(sum2);
+double number9 = 100;
+double number10 = 100;
+double result = number9 / number10;
+Console.WriteLine(result);
+double number11 = 1.5;
+Console.WriteLine(number11);
+double number12 = 99.9;
+number12 = 100;
+Console.WriteLine(number12);
+double number13 = 0.5;
+double number14 = 0.005;
+double result1 = number13 + number14;
+Console.WriteLine(result1);
+double number15 = -1;
+double number16 = 1;
+double result2 = number15 + number16;
+Console.WriteLine(result2);
+double number17 = 100;
+double result3 = result2 / number17;
+Console.WriteLine(result3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

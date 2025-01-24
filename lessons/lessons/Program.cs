9ccf43cc09ca3d6ef8b3bf114af13b37abd4d80a
Roman <rlsempire@outlook.de>
2025-01-24 18:20:46 +0100

    многократные действия

--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -1,102 +1,42 @@
 ﻿
-Console.WriteLine("what is your name?");
-var Admin = "yes";
-if (Admin == "yes")
+for(var position = 0; position < 100; position = position + 1)
 {
-    Console.WriteLine("are you Admin?");
+    Console.WriteLine(position);
 }
 
-Console.WriteLine("Enter your gender (male or female):");
-var gender = Console.ReadLine();
-
-if (gender == "male")
+for(var position = 1; position <100; position++)
 {
-    Console.WriteLine("are you married? (yes or no):");
-    var isMarried = Console.ReadLine();
-
-    if (isMarried == "yes")
-    {
-        Console.WriteLine("you are married.");
-    }
-    else if (isMarried == "no")
-    {
-        Console.WriteLine("yor are not married.");
-    }
+    Console.WriteLine($"Line:{position}");
 }
-else if (gender == "female")
-{
-    Console.WriteLine("are you married? (yes or no):");
-    var isMarried = Console.ReadLine();
 
-    if (isMarried == "yes")
+for (var position = 0; position <= 50; position++)
+{
+    if ((position >= 10 && position <= 20) || (position >= 40 && position <= 50))
     {
-        Console.WriteLine("you are married.");
-    }
-    else if (isMarried == "no")
-    {
-        Console.WriteLine("you are not married.");
+        Console.WriteLine(position);
     }
 }
 
-Console.WriteLine("How old are you?");
-var ageInput = Console.ReadLine();
-var age = Int32.Parse(ageInput);
-if (age < 18)
+var sum = 0;
+for (var position = 0; position < 1000; position++)
 {
-    Console.WriteLine("Access Denied");
-}
-else
-{
-    Console.WriteLine("Access Granted");
-}
+    sum += position;
+    if (sum > 100000)
+    {
+        Console.WriteLine($"The total amount is more than 100000:{position}");
+        break;
+    }
 
-Console.WriteLine("where do you live?");
-var country = Console.ReadLine();
-if(country=="Russian Federation")
-{
-    Console.WriteLine("You live in Russian Federation");
-}
-else if (country == "China")
-{
-    Console.WriteLine("You live in China");
-}
-else if (country == "Turkey")
-{
-    Console.WriteLine("You live in Turkey");
-}
-else
-{
-    Console.WriteLine($"You live in country:{country}");
 }
 
-   
+for (var position = 0; position <= 100; position++)
+{
+    if ((position / 2) * 2 == position)
+        Console.WriteLine(position);
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 

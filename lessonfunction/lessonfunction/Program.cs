b0fd66e7aff1cde4c39dda29d2aeb117b8c18896
Roman <rlsempire@outlook.de>
2025-02-07 15:27:46 +0100

    07.02
    1-9

---
+++ lessonfunction/lessonfunction/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your lastName: ");
+string lastName = GetLastName();
+Console.WriteLine("Your lastName: " + lastName);
+string GetLastName()
+{
+    return Console.ReadLine();
+}
+
--- lessons/lessons/Program.cs
+++ lessons/lessons/Program.cs
@@ -1,42 +1,100 @@
-﻿
-for(var position = 0; position < 100; position = position + 1)
+﻿using System.ComponentModel.Design;
+
+var firstName = GetUserName();
+static string GetUserName()
 {
-    Console.WriteLine(position);
+    Console.WriteLine("What is your firstName?");
+    return Console.ReadLine();
 }
 
-for(var position = 1; position <100; position++)
+var lastName = GetUserlastName();
+static string GetUserlastName()
 {
-    Console.WriteLine($"Line:{position}");
+    Console.WriteLine("what is your lastName?");
+    return Console.ReadLine();
 }
 
-for (var position = 0; position <= 50; position++)
+int age = GetUserAge();
+static int GetUserAge()
 {
-    if ((position >= 10 && position <= 20) || (position >= 40 && position <= 50))
+    Console.Write("How old are you? ");
+    int age;
+    while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
     {
-        Console.WriteLine(position);
+        Console.Write("Invalid input. Please enter a valid age: ");
+    }
+    return age;
+}
+
+var gender = GetUserGender();
+static string GetUserGender()
+{
+    Console.WriteLine("What is your gender? (Male/Female):");
+    return Console.ReadLine();
+}
+
+var isMarried = IsUserMarried();
+static bool IsUserMarried()
+{
+    Console.Write("Are you married? (Yes/No):");
+    var answer = Console.ReadLine().ToLower();
+    return answer == "Yes";
+}
+
+bool likesCoffee = LikesCoffee();
+Console.WriteLine("Likes Coffee:" + likesCoffee);
+static bool LikesCoffee()
+{
+    Console.WriteLine("Do you like Coffee? (Yes/No):");
+    string answer = Console.ReadLine();
+    if (answer == "Yes")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
-var sum = 0;
-for (var position = 0; position < 1000; position++)
+string allergies = GetAllergies();
+Console.WriteLine("Allergie Information:" + allergies);
+static string GetAllergies()
 {
-    sum += position;
-    if (sum > 100000)
+    Console.WriteLine("Do you have allergies? (Yes/No):");
+    string answer = Console.ReadLine();
+    if (answer == "Yes")
     {
-        Console.WriteLine($"The total amount is more than 100000:{position}");
-        break;
+        Console.WriteLine("What are you allergic to?");
+        return Console.ReadLine();
     }
-
+    return "No allergies";
 }
 
-for (var position = 0; position <= 100; position++)
+var marriage = GetMarriageCount();
+static int GetMarriageCount()
 {
-    if ((position / 2) * 2 == position)
-        Console.WriteLine(position);
+    Console.Write("How many times have you been married? (0-100): ");
+    int count;
+    while (!int.TryParse(Console.ReadLine(), out count) || count < 0 || count > 100)
+    {
+        Console.Write("Invalid input. Please enter a number between 0 and 100: ");
+    }
+    return count;
 }
 
 
-  
+string compliment = ComplimentUser();
+Console.WriteLine(compliment);
+static string ComplimentUser()
+{
+    Console.WriteLine("What is your name?");
+    string name = Console.ReadLine();
+    return "Dear" +  name  + ",you look beautiful as always today!";
+}
+
+
+
 
 
 
